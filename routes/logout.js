e041467c27c5d4fd8b664521d654eb86f4e36889
Kenta.u <haitekuhei@gmail.com>
2016-11-05 12:40:27 +0900

    2016/11/05 12:40 update

--- routes/logout.js
+++ routes/logout.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  　delete req.session.user_id;
-    res.render('logout');
+  if (req.session.user_id) {
+    delete req.session.user_id;
+    res.render('logout');//ログイン済みなのでリダイレクト
+  } else {
+    res.redirect('/');//ログインしていないのでリダイレクト
+  }
 });
 
 module.exports = router;
