be366a04f976d00a3f1087ce1fbb80a44abbbd14
Kenta.U <kd1188729@st.kobedenshi.ac.jp>
2016-11-07 09:37:39 +0900

    2016/11/07 optimized code.

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var session = require('express-session');
 var connect = require('connect');
 var ConnectCouchDB = require('connect-couchdb')(session);
 var store = new ConnectCouchDB({ //セッション管理用DB接続設定
-  name: 'sessiondata',
-  username: '',
-  password: '',
-  host: 'localhost',
+    name: 'sessiondata',
+    username: '',
+    password: '',
+    host: 'localhost',
 });
 
 
@@ -23,29 +23,31 @@ var app = express();
 console.log('Example app listening at http://localhost');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));　//joinは結合（__dirnameはソースが入っているディレクトリを表す）
+app.set('views', path.join(__dirname, 'views'));　 //joinは結合（__dirnameはソースが入っているディレクトリを表す）
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({         // cookieに書き込むsessionの仕様を定める
-  secret: 'ajax-hohoho',               // 符号化。改ざんを防ぐ
-  store: store,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: false,
-    maxAge: 60 * 60 * 1000 //60s*60m*1000ms ＝ 1day.
-  }
+app.use(session({ // cookieに書き込むsessionの仕様を定める
+    secret: 'ajax-hohoho', // 符号化。改ざんを防ぐ
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: false,
+        maxAge: 60 * 60 * 1000 //60s*60m*1000ms ＝ 1day.
+    }
 }));
 
 //ページを追加する場合に追加で記述
-app.use('/', routes.homepage);//ページへのルートを記す(新規追加の場合はindex.jsファイル内の配列に追加)
+app.use('/', routes.homepage); //ページへのルートを記す(新規追加の場合はindex.jsファイル内の配列に追加)
 app.use('/users', routes.users);
 app.use('/hoge', routes.hoge);
 app.use('/contents', routes.home);
@@ -58,9 +60,9 @@ app.use('/success', routes.success);
 
 // 404が返ってきた場合の処理
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -68,23 +70,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/login_check.js
+++ routes/login_check.js
@@ -8,33 +8,33 @@ var createhash = require('../public/js/Kfolder/createhash.js').createhash;
 var DB_PORT = "5984";
 var DB_ADDRESS = "http://localhost:";
 var nano = require('nano')(DB_ADDRESS + DB_PORT);
-var userdata = nano.db.use('userdata');//スコープの設定(この状態だとuserdataにスコープがある)
+var userdata = nano.db.use('userdata'); //スコープの設定(この状態だとuserdataにスコープがある)
 
-var STRETCH = 10000;//パスワードをストレッチする際の回数
+var STRETCH = 10000; //パスワードをストレッチする際の回数
 
 router.post('/', function(req, res, next) {
- if(req.body.id !== null && req.body.password !== null){
-   var id = req.body.id; // public/login.htmlのformから飛ばされた情報を受け取って変数に格納
-   var password = req.body.password; //上と同じ
-   userdata.get(id,function(err, jsonobj) { //フォームに入力されたIDと同名のドキュメントをコレクションuserdataから探してくる
-    if(err){//見つからなかった場合の処理
-      res.redirect('/login');
-    }
-    if (!err) { //見つかった場合
-      var dbpass =  jsonobj.hashpass;//jsonオブジェクトからhashpassを参照し変数に格納
-      var salt = jsonobj.salt;
-      var passhash = createhash.method(password, salt, STRETCH);
-      if(dbpass === passhash){
-        req.session.user_id = id;
-        res.redirect('/contents');
-      }else{
+    if (req.body.id !== null && req.body.password !== null) {
+        var id = req.body.id; // public/login.htmlのformから飛ばされた情報を受け取って変数に格納
+        var password = req.body.password; //上と同じ
+        userdata.get(id, function(err, jsonobj) { //フォームに入力されたIDと同名のドキュメントをコレクションuserdataから探してくる
+            if (err) { //見つからなかった場合の処理
+                res.redirect('/login');
+            }
+            if (!err) { //見つかった場合
+                var dbpass = jsonobj.hashpass; //jsonオブジェクトからhashpassを参照し変数に格納
+                var salt = jsonobj.salt;
+                var passhash = createhash.method(password, salt, STRETCH);
+                if (dbpass === passhash) {
+                    req.session.user_id = id;
+                    res.redirect('/contents');
+                } else {
+                    res.redirect('/login');
+                }
+            }
+        });
+    } else {
         res.redirect('/login');
-      }
     }
- });
- }else{
-   res.redirect('/login');
-}
 });
 
 
