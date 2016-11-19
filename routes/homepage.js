f0d931255779b19325f4999d130459525b3bb31b
Kenta.u <haitekuhei@gmail.com>
2016-11-19 17:03:15 +0900

    update login system

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // ページを追加する(静的コンテンツの追加)
 router.get('/', function(req, res, next) {
-    res.sendFile(process.cwd() + "/public/products/マイページ/index.html"); //静的コンテンツの参照(絶対パス)
+    res.sendFile(process.cwd() + "/public/products/index.html"); //静的コンテンツの参照(絶対パス)
 });
 
 module.exports = router;
