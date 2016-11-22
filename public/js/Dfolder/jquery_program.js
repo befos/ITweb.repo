150f3884e79fb649ce7fc5a99eda74ffb6f9ab75
Daichi <zeldagb428@gmail.com>
2016-11-22 11:33:43 +0900

    定期アップデート

--- public/js/Dfolder/jquery_program.js
+++ public/js/Dfolder/jquery_program.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
+$(function() {
 $('.glaybox').fadeIn(2000);
-});
 /*ページトップへ戻る Jquery*/
-$(function(){
   var pageTop = $(".toTop");
   pageTop.click(function () {
     $('body, html').animate({ scrollTop: 0 }, 300);
@@ -15,21 +13,18 @@ $(function(){
       pageTop.fadeOut();
     }
   });
-});
-
-$(function(){
-    $(window).scroll(function (){
-        $('.fadein').each(function(){
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200){
-                $(this).addClass('scrollin');
-            }
-        });
+// コンテンツのフェードイン
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200){
+      $(this).addClass('scrollin');
+      }
     });
+  });
 });
-
 
 
 /*バリデーション validation engine*/
@@ -42,17 +37,3 @@ $(function(){
 //     scroll: false
 //   });
 // })(jQuery);
-//
-// // ログインボタンの処理
-// $(document).ready(function(){
-//   $(function() {
-//       $('#Loginbtn li img').hover(
-//           function(){
-//               $(this).stop().animate({'marginTop':'-29px'},'fast');
-//           },
-//           function () {
-//               $(this).stop().animate({'marginTop':'0px'},'fast');
-//           }
-//       );
-//   });
-// });
