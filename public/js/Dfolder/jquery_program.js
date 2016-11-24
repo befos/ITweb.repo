1d1cea6799540584eec70127234b4f0ba0653920
Daichi <zeldagb428@gmail.com>
2016-11-24 15:24:56 +0900

    update

--- public/js/Dfolder/jquery_program.js
+++ public/js/Dfolder/jquery_program.js
@@ -1,29 +1,31 @@
 $(function() {
-$('.glaybox').fadeIn(2000);
-/*ページトップへ戻る Jquery*/
-  var pageTop = $(".toTop");
-  pageTop.click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 300);
-    return false;
-  });
-  $(window).scroll(function () {
-    if($(this).scrollTop() >= 200) {
-      pageTop.fadeIn();
-    } else {
-      pageTop.fadeOut();
-    }
-  });
-// コンテンツのフェードイン
-  $(window).scroll(function (){
-    $('.fadein').each(function(){
-      var elemPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > elemPos - windowHeight + 200){
-      $(this).addClass('scrollin');
-      }
+    $('.glaybox').fadeIn(2000);
+    /*ページトップへ戻る Jquery*/
+    var pageTop = $(".toTop");
+    pageTop.click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
     });
-  });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 200) {
+            pageTop.fadeIn();
+        } else {
+            pageTop.fadeOut();
+        }
+    });
+    // コンテンツのフェードイン
+    $(window).scroll(function() {
+        $('.fadein').each(function() {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
 });
 
 
