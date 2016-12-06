71631e07f0caf5adc05e98e92089ddd860e775f4
Kenta.u <haitekuhei@gmail.com>
2016-12-06 21:23:57 +0900

    csp_out job

--- public/js/Kfolder/output_js/emailchange.js
+++ public/js/Kfolder/output_js/emailchange.js
@@ -1,6 +1,6 @@
 function conrimMessage() {
   var id = document.getElementById("id").value;
-  var uid = document.getElementById("udi").value;
+  var uid = document.getElementById("uid").value;
  //必須チェック
  if(id == "" || uid =="") {
     alert("必須項目が入力されていません。");
