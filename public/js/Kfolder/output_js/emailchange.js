af6788f1a3a4a97ab5f401eb3136372ccbf5b2a3
Kenta.u <haitekuhei@gmail.com>
2016-12-06 18:54:37 +0900

    Securty update

---
+++ public/js/Kfolder/output_js/emailchange.js
@@ -0,0 +1,11 @@
+function conrimMessage() {
+  var id = document.getElementById("id").value;
+  var uid = document.getElementById("udi").value;
+ //必須チェック
+ if(id == "" || uid =="") {
+    alert("必須項目が入力されていません。");
+    return false;
+ }
+
+  return true;
+}
