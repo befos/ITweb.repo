19d0fda7cb87bb77c002ff2d9961a656dda23b91
Kenta.u <haitekuhei@gmail.com>
2016-12-12 07:28:31 +0900

    small update

--- routes/question_board_submit.js
+++ routes/question_board_submit.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+require('date-utils');//現在時刻の取得
 
 //データベース接続および設定
 var mongoose = require('mongoose');
@@ -9,30 +10,32 @@ var ForumCont = models.ForumCont;
 
 router.post('/', function(req, res, next) {
     //test用
-    var foname = "test";
-    var host = req.session.user_id;
+    var foname = "C言語難しすぎンゴ・・・";
+    var host = req.session.obj_id;
     var question ='<p>むずすぎぃぃ！！</p>'
     var dummytext = '<p>ほげげ</p>';
     var name = "testユーザー";
     var tag = ["C言語", "難しい"];
     //↑ダミー
+    var dt = new Date();
+    var uday = dt.toFormat("YYYY/MM/DD HH24:MI:SS");
     mongoose.connect('mongodb://localhost:27017/userdata');
     var makeforum = new Forum({
         foname: foname,//フォーラムの名前（被りあり）
-        host: host,//uid
+        host: host,//obj_id
         count: null,//アクセスされた回数
-        uday: null,
+        uday: uday,
         ques: question,
         tag: tag,//多分500要素まで？この中に言語も記述してもらう(ニコ動のタグみたいなもの)
-        f_st: true//forumの内容が解決済みか
+        f_st: true//forumの内容が解決済み...false　初期値はtrue
     });
     makeforum.save(function(err) {//refを使用しているの二重にセーブ
       if(err) return hadDbError(err , req, res);//バリデーションエラーが出る可能性(もし被りが出た場合)
       var makefocont = new ForumCont({
-          uid: req.session.user_id,
-          name: name,//ユーザーが決めた自由な名前
+          uid: req.session.obj_id,
+          name: req.session.user_name,//ユーザーが決めた自由な名前
           prop: null,//プロフィールの画像？
-          cuday: null,//コンテンツを上げた日
+          cuday: uday,//コンテンツを上げた日
           chday: null,//内容を編集した日
           text: dummytext//本文
       });
