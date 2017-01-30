a77db6f8ee20441b7bf7e9a2a935015fefbe1109
Kenta.u <haitekuhei@gmail.com>
2017-01-30 16:59:15 +0900

    ok

--- routes/contact_submit.js
+++ routes/contact_submit.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mailer = require('nodemailer');
 var generator = require('xoauth2').createXOAuth2Generator({ //googleの認証用
-    user: 'stitches01@gmail.com',
+    user: 'stichies01@gmail.com',
     clientId: '1096218509599-63cs90qmsvdg5v8to44cn3tgl4ni0c9o.apps.googleusercontent.com',
     clientSecret: 'XMkfmFGd2Iv1jBWNgvmjUxsf',
     refreshToken: '1/gSZzfoVBTjXr1IE-ah-n7mA3aLl3RulrQHItdoznRkw',
--- routes/email_change_mail.js
+++ routes/email_change_mail.js
@@ -4,7 +4,7 @@ var randword = require('../public/js/Kfolder/randword.js').randword;
 var sha256 = require('js-sha256');
 var mailer = require('nodemailer');　
 var generator = require('xoauth2').createXOAuth2Generator({ //googleの認証用
-    user: 'stitches01@gmail.com',
+    user: 'stichies01@gmail.com',
     clientId: '1096218509599-63cs90qmsvdg5v8to44cn3tgl4ni0c9o.apps.googleusercontent.com',
     clientSecret: 'XMkfmFGd2Iv1jBWNgvmjUxsf',
     refreshToken: '1/gSZzfoVBTjXr1IE-ah-n7mA3aLl3RulrQHItdoznRkw',
